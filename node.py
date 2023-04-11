f83eb01ca159fb2f95454daf9e9d8011b57f3795
devloom <dloom@umich.edu>
2023-04-11 12:48:55 -0400

    updates to tree and node class

--- node.py
+++ node.py
@@ -1,8 +1,5 @@
 class Node:
-	def __init__(self, gini, num, num_samples_per_class, pred_class):
-		self.gini = gini
-		self.num = num
-		self.num_per_class = num_per_class
+	def __init__(self, pred_class):
 		self.pred_class = pred_class
 		self.row_index = 0
 		self.col_index = 0
--- tree.py
+++ tree.py
@@ -1,13 +1,17 @@
 from node import Node
+from dataset import Dataset
+from node import Node
+
+import numpy as np
 
 class Tree():
-    def __init__(self,dataset):
+    def __init__(self,data):
         super().__init__()
 
-        self.depth = 5
+        self.max_depth = 2
         self.splittingFunction = 'gini'
-        self.n_classes = len(set(dataset.train_y))
-        self.nodes = self.grow(dataset.train_x,dataset.train_y)
+        self.n_classes = len(set(data.train_y))
+        self.nodes = self.grow(data.train_x,data.train_y)
 
         #self.createTrees()
 
@@ -32,9 +36,12 @@ class Tree():
     '''
     
     #determine best split that specifies col #, row #, (r,g,or,b), threshold for each node
+    # this still needs to be written
+    # we need to discuss best way to go about splitting
     def splitter(self,X,y):
         best_col = -1
         best_row = -1
+        best_row = 1
         best_rgb = 0
         best_thr = 0.5
         return best_col, best_row, best_rgb, best_thr
@@ -44,11 +51,13 @@ class Tree():
     def grow(self,X,y,depth=0):
         num_samples_per_class = [np.sum(y == i) for i in range(self.n_classes)]
         predicted_class = np.argmax(num_samples_per_class)
+        print(predicted_class)
         node = Node(pred_class=predicted_class)
         if depth < self.max_depth:
             rowIdx, colIdx, rgbIdx, thr = self.splitter(X, y)
             indices_left = [False]*len(y)
-            if idx is not None:
+            if (colIdx > 0 or rowIdx > 0):
+            #if idx is not None:
                 for i in range(len(y)):
                     img = dataset.imgNumpy(i)
                     if (rowIdx < 0 and np.mean(img[:,colIdx,rgbIdx]) < thr):
@@ -66,3 +75,6 @@ class Tree():
             
             
 
+if __name__ == '__main__':
+    dataset = Dataset()
+    tree = Tree(dataset)
